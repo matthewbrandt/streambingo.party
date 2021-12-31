dbf9f010d3ece712e3a9783eea646848b93151d9
Matthew Brandt <matthewbrandt@gmail.com>
2021-12-31 01:15:18 +0100

    board config and seed function ready

--- app.js
+++ app.js
@@ -1,56 +1,73 @@
 // [p] = passive (occurs)
 // [c] = chat
 // [s] = streamer
-
-const tileMessage = [
-    [1,"[c]","3+ subscriptions"],
-    [2,"[c]","500+ bits"],
-    [3,"[c]","chat spam (buy follows)"],
-    [4,"[c]","comment on streamers appearance"],
-    [5,"[c]","user banned"],
-    [6,"[c]","user timeout"],
-    [7,"[p]","gets SUPER lucky"],
-    [8,"[p]","technical difficulties"],
-    [9,"[s]","does a voice impression"],
-    [10,"[s]","drinks water"],
-    [11,"[s]","forgets a follow/sub/etc."],
-    [12,"[s]","forgets to change scene"],
-    [13,"[s]","forgets to mute mic"],
-    [14,"[s]","gets raided"],
-    [15,"[s]","mispronounces someone's username"],
-    [16,"[s]","OBS/stream crashes"],
-    [17,"[s]","runs a poll"],
-    [18,"[s]","says \"i can't believe that worked\""],
-    [19,"[s]","says \"let's goooooo\""],
-    [20,"[s]","says \"what happened?!\""],
-    [21,"[s]","says to join discord"],
-    [22,"[s]","sings a little"],
-    [23,"[s]","stands up"],
-    [24,"[s]","touches microphone by mistake"]
+const tilesArray = [
+    "3+ subscriptions",
+    "500+ bits",
+    "chat spam (e.g. buy follows)",
+    "comment on streamers appearance",
+    "user banned",
+    "user timeout",
+    "gets SUPER lucky",
+    "technical difficulties",
+    "does a voice impression",
+    "drinks water",
+    "forgets a notification",
+    "forgets to change scene",
+    "forgets to mute mic",
+    "gets raided",
+    "mispronounces someone's username",
+    "OBS/stream crashes",
+    "runs a poll",
+    "says \"i can't believe that worked\"",
+    "says \"let's goooooo\"",
+    "says \"what happened?!\"",
+    "says to join discord",
+    "sings a little",
+    "stands up",
+    "touches microphone by mistake"
 ];
 
-// get the entire array
-// push a random value into each array grouping (math.random0-1)
-// sort array by value
-// populate tiles one by one with loop through array
-// generate uuid for this particular combination and SAVE
-
-function getMessages() {
-    messageArray = [];
-
-    for (let i = 0; i < tileMessage.length; i++) {
-        // do nothing yet
-        let rn = Math.random();
-        // console.log(tileMessage[i][1]);
-        messageArray.push([rn,tileMessage[i][0],tileMessage[i][2]]);
-        // console.log(messageArray);
+// taken from https://stackoverflow.com/questions/16801687/javascript-random-ordering-with-seed
+function shuffle(array, seed) {                
+    var m = array.length, t, i;
+  
+    // While there remain elements to shuffle…
+    while (m) {
+  
+      // Pick a remaining element…
+      i = Math.floor(random(seed) * m--);        
+  
+      // And swap it with the current element.
+      t = array[m];
+      array[m] = array[i];
+      array[i] = t;
+      ++seed                                     
     }
+  
+    return array;
+  }
+  
+  function random(seed) {
+    var x = Math.sin(seed++) * 10000; 
+    return x - Math.floor(x);
+  }
 
-    bingoArray = messageArray.sort();
-    // console.log(bingoArray);
-    
+let boardConfig = shuffle(tilesArray,69);
+console.log(boardConfig);
+
+// DO NOT EVER ADD ANOTHER TABLE TO THE PAGE
+function getMessages() {
+    //let tableLength = document.getElementsByTagName("td").length;
+    for (let i = 0; i < boardConfig.length; i++) {
+        if (i < 12) { 
+            document.getElementsByTagName("td")[i].innerText = boardConfig[i]; 
+        }
+        if (i >= 12) {
+            document.getElementsByTagName("td")[i+1].innerText = boardConfig[i];
+        }
+    }
 }
-
 getMessages();
 
 //https://pusher.com/docs/channels/getting_started/javascript/
